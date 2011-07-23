1b5433ae8a0af11d9e345e81a72b341978a38f37
Gabor Angeli <angeli@cs.stanford.edu>
2011-07-23 16:23:14 -0700

    javaNLP interface fixes

--- src/org/goobs/slib/JavaNLP.scala
+++ src/org/goobs/slib/JavaNLP.scala
@@ -1,32 +1,36 @@
 package org.goobs.slib
 
 import edu.stanford.nlp.util.CoreMap
-import edu.stanford.nlp.util.ArrayCoreMap
+import edu.stanford.nlp.ling.CoreLabel
 import edu.stanford.nlp.ling.CoreAnnotations.TextAnnotation
 import edu.stanford.nlp.ling.CoreAnnotations.PartOfSpeechAnnotation
 import edu.stanford.nlp.ling.CoreAnnotations.LemmaAnnotation
+import edu.stanford.nlp.ling.CoreAnnotations.AnswerAnnotation
 
 import scala.collection.JavaConversions._
 
+import java.util.AbstractList
+
 object JavaNLP {
-	val WORD  = classOf[TextAnnotation]
-	val POS   = classOf[PartOfSpeechAnnotation]
-	val LEMMA = classOf[LemmaAnnotation]
+	val WORD   = classOf[TextAnnotation]
+	val POS    = classOf[PartOfSpeechAnnotation]
+	val LEMMA  = classOf[LemmaAnnotation]
+	val ANSWER = classOf[AnswerAnnotation]
 
 
 	def word2coremap(word:String):CoreMap = {
-		val token = new ArrayCoreMap(1)
+		val token = new CoreLabel(2)
 		token.set(WORD,word)
 		token
 	}
 	def word2coremap(word:String,pos:String):CoreMap = {
-		val token = new ArrayCoreMap(2)
+		val token = new CoreLabel(3)
 		token.set(WORD,word)
 		token.set(POS,pos)
 		token
 	}
 	def word2coremap(word:String,pos:String,lemma:String):CoreMap = {
-		val token = new ArrayCoreMap(2)
+		val token = new CoreLabel(4)
 		token.set(WORD,word)
 		token.set(POS,pos)
 		token.set(LEMMA,lemma)
@@ -50,4 +54,28 @@ object JavaNLP {
 		sent.zip(pos).zip(lemma).map{ case ((w:String,pos:String),lemma:String) => 
 			word2coremap(w,pos,lemma) }.toList
 	}
+
+	def annotate(sent:java.util.List[CoreMap],tags:Array[String]
+			):java.util.List[CoreMap] = {
+		//(check)
+		if(sent.size != tags.length){ 
+			throw new IllegalArgumentException("Length mismatch")
+		}
+		//(tag)
+		var i:Int = 0
+		val iter:java.util.Iterator[CoreMap] = sent.iterator
+		while(iter.hasNext){
+			iter.next.set(ANSWER,tags(i))
+			i += 1
+		}
+		sent
+	}
+
+	//--Implicits Magic--
+	implicit def coremapList2words(lst:AbstractList[_<:CoreMap]):Array[String] = {
+		lst.map{ (term:CoreMap) => 
+			term.get[String,TextAnnotation](WORD)
+		}.toArray
+	}
 }
+
