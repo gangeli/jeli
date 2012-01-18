0369eac3d486f3e97ccbd7492f334e74af966d30
Gabor Angeli <angeli@cs.stanford.edu>
2012-01-18 01:29:13 -0800

    CKY tests added

---
+++ test/src/org/goobs/tests/CKYTest.scala
@@ -0,0 +1,194 @@
+package org.goobs.tests
+
+import org.scalatest.Spec
+import org.scalatest.matchers.ShouldMatchers
+import org.goobs.nlp._
+
+object Grammars {
+	lazy val TOY:Array[GrammarRule] = {
+		Array[GrammarRule](
+			BinaryGrammarRule(NodeType.ROOT, NodeType.make("S")),
+			BinaryGrammarRule(NodeType.make("S"), NodeType.make("NP"), NodeType.make("VP")),
+			BinaryGrammarRule(NodeType.make("NP"), NodeType.makePreterminal("NN")),
+			BinaryGrammarRule(NodeType.make("VP"), NodeType.makePreterminal("VB"), NodeType.make("NP")),
+		  LexGrammarRule(NodeType.makePreterminal("NN")),
+			LexGrammarRule(NodeType.makePreterminal("VB"))
+		)
+	}
+	lazy val w2str:Array[String] = Array[String](
+	  "I",
+		"like",
+		"sugar",
+		"NLP"
+		)
+}
+
+class CKYParserSpec extends Spec with ShouldMatchers {
+	import Grammars._
+	
+	describe("A Node Type"){
+		it("should be createable"){
+			//(simple equality)
+			NodeType.make("S") should be (NodeType('S))
+			NodeType.make("S") should be (NodeType.make('S))
+			//(with flags)
+			NodeType.make("NN",NodeType.SYM_PRETERMINAL) should be (NodeType('NN))
+			//(constants)
+			NodeType.ROOT
+			NodeType.WORD
+			NodeType.WORD should be (NodeType.makeWord())
+			//(duplicate create)
+			intercept[IllegalArgumentException]{
+				NodeType.make("S", NodeType.SYM_PRETERMINAL)
+			}
+		}
+		it("should recognize flags"){
+			NodeType.WORD.isWord should be (true)
+			NodeType.WORD.isPreterminal should be (false)
+			NodeType.makeWord().isWord should be (true)
+			NodeType.make("NN",NodeType.SYM_PRETERMINAL).isWord should be (false)
+			NodeType.make("NN",NodeType.SYM_PRETERMINAL).isPreterminal should be (true)
+		}
+	}
+	
+	describe("A sentence"){
+		it("should be creatable"){
+			val sent:Sentence = Sentence(w2str,"I like sugar")
+			//(integer gets)
+			sent(0) should be (0)
+			sent(1) should be (1)
+			sent(2) should be (2)
+			//(string gets)
+			sent.gloss(0) should be ("I")
+			sent.gloss(1) should be ("like")
+			sent.gloss(2) should be ("sugar")
+			//(object overrides)
+			sent.toString should be ("I like sugar")
+			sent should be (sent)
+			sent.hashCode should be (sent.hashCode)
+			//(in depth equality check)
+			val sent2:Sentence = Sentence(w2str,"I like sugar")
+			sent should be (sent2)
+			sent.hashCode should be (sent2.hashCode)
+		}
+		it("should be creatable from a corpus"){
+			val corpus:Array[Sentence] = Sentence(Array[String]("I like sugar", "I like NLP"))
+			corpus.length should be (2)
+			corpus(0)(2) should be (2)
+			corpus(1)(2) should be (3)
+			corpus(0) should not be (corpus(1))
+		}
+	}
+	
+	describe("A toy binarized grammar") {
+		it("should be definable"){
+			val toyGrammar = TOY;
+		}
+		it("should binarize"){
+			val topRule = TOY(0);
+			topRule.binarize.foreach{ (rule:CKYRule) =>
+				//(parent)
+				rule.parent should  be (NodeType.ROOT)
+				//(unary check)
+				assert(rule.isUnary)
+				assert(rule.isInstanceOf[CKYUnary])
+				rule.child should be (NodeType("S"))
+				//(not binary check)
+				intercept[AssertionError]{
+					rule.leftChild;
+				}
+				intercept[AssertionError]{
+					rule.rightChild
+				}
+			}
+		}
+		it("should know about its lexical elements"){
+			assert(!TOY(0).binarize.next().isLex)
+			assert(!TOY(1).binarize.next().isLex)
+			assert(!TOY(2).binarize.next().isLex)
+			assert(!TOY(3).binarize.next().isLex)
+			assert(TOY(4).binarize.next().isLex)
+			assert(TOY(5).binarize.next().isLex)
+		}
+		it("should be passable to a parser"){
+			CKYParser(w2str.length, TOY) should not be (null)
+			CKYParser(w2str.length, TOY:_*) should not be (null)
+		}
+		it("should have a proper rule distribution"){
+			val parser = CKYParser(w2str.length, TOY);
+			//(should have probabilities)
+			parser.ruleProb(new CKYBinary(NodeType('S), NodeType('NP), NodeType('VP))) should be (1.0)
+			parser.ruleProb(new CKYBinary(NodeType('VP), NodeType('VB), NodeType('NP))) should be (1.0)
+			parser.ruleProb(new CKYUnary(NodeType.ROOT, NodeType('S))) should be (1.0)
+			parser.ruleProb(new CKYUnary(NodeType('NP), NodeType('NN))) should be (1.0)
+			//(should not have probabilities)
+			intercept[NoSuchElementException]{
+				parser.ruleProb(new CKYUnary(NodeType('NN), NodeType.WORD))
+			}
+			intercept[NoSuchElementException]{
+				parser.ruleProb(new CKYUnary(NodeType('VB), NodeType.WORD))
+			}
+		}
+		it("should have a proper lex distribution"){
+			val parser = CKYParser(w2str.length, TOY);
+			(0 until w2str.length).foreach{ (w:Int) =>
+				//(should have probabilities)
+				parser.lexProb(new CKYUnary(NodeType('NN), NodeType.WORD), w) should be (1.0 / w2str.length.asInstanceOf[Double])
+				parser.lexProb(new CKYUnary(NodeType('VB), NodeType.WORD), w) should be (1.0 / w2str.length.asInstanceOf[Double])
+				//(should not have probabilities)
+				intercept[NoSuchElementException]{
+					parser.lexProb(new CKYUnary(NodeType.ROOT, NodeType('S)), w)
+				}
+				intercept[NoSuchElementException]{
+					parser.lexProb(new CKYUnary(NodeType('NP), NodeType('NN)), w)
+				}
+			}
+		}
+		it("should parse simple sentences"){
+			val parser = CKYParser(w2str.length, TOY, true);
+			var sent:Sentence = Sentence(w2str, "I like sugar")
+			//(parse beam 1)
+			val parse1 = parser.parse(sent,1);
+			parse1 should not be (null)
+			parse1.length should be (1)
+			parse1(0).parent should be (NodeType.ROOT);
+			parse1(0).asParseString(sent) should not be (null)
+			parser.parse(sent) should not be (null)
+			//(parse beam 100)
+			sent = Sentence(w2str, "I like sugar sugar")
+			val parse100 = parser.parse(sent,100)
+			parse100.length should be > (1)
+			parse100.length should be <= (100)
+			parse100.foreach{ (p:ParseTree) =>
+				p.parent should be (NodeType.ROOT)
+			}
+			//(parse beam 100k)
+			val parse100k = parser.parse(sent,100000)
+			parse100.length should be > (1)
+			parse100.length should be <= (100000)
+			parse100.foreach{ (p:ParseTree) =>
+				p.parent should be (NodeType.ROOT)
+			}
+			//(parse another sentence)
+			val sent2:Sentence = Sentence(w2str, "I like like NLP")
+			val parseOther = parser.parse(sent2,100)
+			parseOther.length should be > (1)
+			parseOther.length should be <= (100)
+			parseOther.foreach{ (p:ParseTree) =>
+				p.parent should be (NodeType.ROOT)
+			}
+		}
+	}
+	
+	describe("a grammar with closures"){
+		//TODO make me
+	}
+	
+	describe("a grammar with ambiguous parses"){
+		//TODO make me
+	}
+	
+	describe("A parser with learned parameters"){
+		//TODO write me
+	}
+}
---
+++ test/src/org/goobs/tests/SearchTest.scala
@@ -0,0 +1,270 @@
+package org.goobs.tests
+
+import org.scalatest.Spec
+import org.scalatest.matchers.ShouldMatchers
+
+import org.goobs.nlp._
+import org.goobs.nlp.Search._
+
+object FakeTree {
+	def node(name:String, childrenLst:List[SearchState],
+			s:Double=0.0,h:Double=0.0) = {
+		new SearchState {
+			override def children:List[SearchState] = childrenLst
+			override def isEndState:Boolean = {childrenLst.length == 0}
+			override def cost:Double = s
+			override def heuristic:Double = h
+			override def toString:String = name
+		}
+	}
+	val node2:SearchState = node("node2", List[SearchState]()           , 5, 0)
+	val node4:SearchState = node("node4", List[SearchState]()           , 8, 1)
+	val node6:SearchState = node("node6", List[SearchState]()           , 6, 0)
+	val node5:SearchState = node("node5", List[SearchState](node6)      , 5, 0)
+	val node3:SearchState = node("node3", List[SearchState](node5)      , 4, 1)
+	val node1:SearchState = node("node1", List[SearchState](node4,node3), 1, 3)
+	val node0:SearchState = node("node0", List[SearchState](node2,node1), 5, 4)
+}
+
+object Manhattan {
+	var EDGE = java.lang.Integer.MAX_VALUE
+}
+class Manhattan(val x:Int,val y:Int,val c:Double) extends SearchState {
+	def this(x:Int,y:Int) = this(x,y,0.0)
+	import scala.math.abs
+	//--Search Overrides
+	override def children:List[Manhattan] 
+		= List[Manhattan](
+			new Manhattan(x-1,y,c+1),new Manhattan(x+1,y,c+1),
+			new Manhattan(x,y-1,c+1),new Manhattan(x,y+1,c+1))
+	override def isEndState:Boolean = x == 0 && y == 0
+	override def cost:Double = c
+	override def heuristic:Double= (abs(x)+abs(y)).asInstanceOf[Double]
+	//--Debug Overrides
+	override def assertDequeueable:Boolean = {
+		x < Manhattan.EDGE && y < Manhattan.EDGE
+	}
+	//--Standard Overrides
+	override def toString:String = "("+x+","+y+":"+c+")"
+	override def equals(o:Any):Boolean = o match {
+			case (m:Manhattan) => (m.x==x && m.y==y)
+			case _ => false
+		}
+	override def hashCode:Int = x.hashCode ^ (y.hashCode << 16)
+}
+
+
+class EuclideanManhattan(x:Int,y:Int,c:Double) extends Manhattan(x,y,c) {
+	def this(x:Int,y:Int) = this(x,y,0.0)
+	override def children:List[EuclideanManhattan] 
+		= List[EuclideanManhattan](
+			new EuclideanManhattan(x-1,y,c+1),new EuclideanManhattan(x+1,y,c+1),
+			new EuclideanManhattan(x,y-1,c+1),new EuclideanManhattan(x,y+1,c+1))
+	override def cost:Double = c
+	override def heuristic:Double= scala.math.sqrt((x*x+y*y).asInstanceOf[Double])
+	//--Debug Overrides
+	override def assertDequeueable:Boolean = {
+		x < Manhattan.EDGE && y < Manhattan.EDGE
+	}
+}
+
+
+class SearchSpec extends Spec with ShouldMatchers{
+	val BREAK = (s:SearchState,c:Int) => false
+	val IGNORE = (s:SearchState,c:Int) => true
+
+
+	describe("DFS") {
+		it("should be creatable"){ 
+			val dfs:Search[SearchState] = Search(DFS)
+		}
+		it("works for Fake Tree"){
+			val dfs:Search[SearchState] = Search(DFS)
+			val results:Array[SearchState] = dfs.search(FakeTree.node0)
+			results.length should be (3)
+			results(0) should be (FakeTree.node6)
+			results(1) should be (FakeTree.node4)
+			results(2) should be (FakeTree.node2)
+		}
+		it("breaks for Manhattan"){
+			val dfs:Search[Manhattan] = Search(DFS)
+			val count = dfs.search(new Manhattan(5,5),BREAK,10000)
+			count should be (10000)
+		}
+	}
+
+
+	describe("BFS") {
+		it("should be creatable"){ 
+			val bfs:Search[SearchState] = Search(BFS)
+		}
+		it("works for Fake Tree"){
+			val bfs:Search[SearchState] = Search(BFS)
+			val results:Array[SearchState] = bfs.search(FakeTree.node0)
+			results.length should be (3)
+			results(0) should be (FakeTree.node2)
+			results(1) should be (FakeTree.node4)
+			results(2) should be (FakeTree.node6)
+		}
+		it("works for Manhattan"){
+			val bfs:Search[Manhattan] = Search(BFS)
+			//(correctness)
+			val out:Manhattan = bfs.best(new Manhattan(5,5))
+			out should be (new Manhattan(0,0))
+			//(time)
+			val count = bfs.search(new Manhattan(5,5),BREAK)
+			count should be > (1000)
+		}
+	}
+
+
+	describe("Uniform Cost Search") {
+		it("should be creatable"){ 
+			val ucs:Search[SearchState] = Search(UNIFORM_COST)
+		}
+		it("works for Fake Tree"){
+			val ucs:Search[SearchState] = Search(UNIFORM_COST)
+			val results:Array[SearchState] = ucs.search(FakeTree.node0)
+			results.length should be (3)
+			results(0) should be (FakeTree.node2)
+			results(1) should be (FakeTree.node6)
+			results(2) should be (FakeTree.node4)
+			ucs.search(FakeTree.node0, (s,c)=>false) should be (4)
+		}
+		it("works for Manhattan"){
+			val ucs:Search[Manhattan] = Search(UNIFORM_COST)
+			//(correctness)
+			val out:Manhattan = ucs.best(new Manhattan(5,5))
+			out should be (new Manhattan(0,0))
+			//(time)
+			val count = ucs.search(new Manhattan(5,5),BREAK)
+			count should be > (1000)
+			//(consistency with BFS)
+			val bfs:Search[Manhattan] = Search(BFS)
+			val bfsCount:Int = bfs.search(new Manhattan(5,5),BREAK)
+			(scala.math.abs(count - bfsCount) < (0.05*count)) should be (true)
+		}
+	}
+
+
+	describe("AStar (uncached)") {
+		it("should be creatable"){ 
+			val astar:Search[SearchState] = Search(A_STAR)
+		}
+		it("works for Fake Tree"){
+			val astar:Search[SearchState] = Search(A_STAR)
+			val results:Array[SearchState] = astar.search(FakeTree.node0)
+			results.length should be (3)
+			results(0) should be (FakeTree.node2)
+			results(1) should be (FakeTree.node6)
+			results(2) should be (FakeTree.node4)
+			astar.search(FakeTree.node0, (s,c)=>false) should be (3) //pruning
+		}
+		it("works for Manhattan"){
+			val astar:Search[Manhattan] = Search(A_STAR)
+			//(correctness)
+			val out:Manhattan = astar.best(new Manhattan(5,5))
+			out should be (new Manhattan(0,0))
+			//(time)
+			var count = astar.search(new Manhattan(5,5),BREAK)
+			count should be < (100)
+			//(mid time)
+			count = astar.search(new Manhattan(10,10),BREAK)
+			count should be < (2000)
+		}
+		it("breaks for Manhattan (large)"){
+			val astar:Search[Manhattan] = Search(A_STAR)
+			//(long time)
+			val count = astar.search(new Manhattan(100,100),BREAK,100000)
+			count should be (100000)
+		}
+	}
+
+
+	describe("AStar (cached)"){
+		it("should be creatable"){ 
+			val astar:Search[SearchState] = Search(cache(A_STAR))
+		}
+		it("works for Fake Tree"){
+			val astar:Search[SearchState] = Search(cache(A_STAR))
+			val results:Array[SearchState] = astar.search(FakeTree.node0)
+			results.length should be (3)
+			results(0) should be (FakeTree.node2)
+			results(1) should be (FakeTree.node6)
+			results(2) should be (FakeTree.node4)
+			astar.search(FakeTree.node0, (s,c)=>false) should be (3) //pruning
+		}
+		it("works for Manhattan"){
+			val astar:Search[Manhattan] = Search(cache(A_STAR))
+			//(correctness)
+			val out:Manhattan = astar.best(new Manhattan(5,5))
+			out should be (new Manhattan(0,0))
+			//(time)
+			var count = astar.search(new Manhattan(5,5),BREAK)
+			count should be < (100)
+			//(mid time)
+			count = astar.search(new Manhattan(10,10),BREAK)
+			count should be < (200)
+		}
+		it("works for Manhattan (large)"){
+			val astar:Search[Manhattan] = Search(cache(A_STAR))
+			//(long time)
+			Manhattan.EDGE=102
+			val count = astar.search(new Manhattan(100,100),BREAK)
+			count should be < (10000)
+			Manhattan.EDGE=java.lang.Integer.MAX_VALUE
+		}
+		it("works for Imprecise Manhattan"){
+			val astar:Search[EuclideanManhattan] = Search(cache(A_STAR))
+			//(long time)
+			val count = astar.search(new EuclideanManhattan(100,100),BREAK)
+			count should be < (100000)
+			//(better than ucs)
+			val ucs:Search[EuclideanManhattan] = Search(cache(UNIFORM_COST))
+			val ucsCount = ucs.search(new EuclideanManhattan(100,100),BREAK)
+			count should be < (ucsCount)
+		}
+	}
+	
+	
+	describe("Beam"){
+		it("should be creatable"){ 
+			val beamDFS:Search[SearchState] = Search(memcap(DFS,10000,0.5))
+			val beamBFS:Search[SearchState] = Search(memcap(BFS,10000,0.5))
+			val beamUCS:Search[SearchState] = Search(memcap(UNIFORM_COST,10000,0.5))
+			val beamASTAR:Search[SearchState] = Search(memcap(A_STAR,10000,0.5))
+		}
+		it("actually caps (Fake Tree)"){
+			val beamDFS:Search[SearchState] = Search(memcap(DFS,2,0.5))
+			val results:Array[SearchState] = beamDFS.search(FakeTree.node0)
+			results.length should be (1)
+			results(0) should be (FakeTree.node6)
+		}
+		it("works for Fake Tree"){
+			val beamDFS:Search[SearchState] = Search(memcap(DFS,4,0.5))
+			val results:Array[SearchState] = beamDFS.search(FakeTree.node0)
+			results.length should be (3)
+			results(0) should be (FakeTree.node6)
+			results(1) should be (FakeTree.node4)
+			results(2) should be (FakeTree.node2)
+		}
+		it("works for Manhattan"){
+			val astar:Search[Manhattan] = Search(memcap(A_STAR,200,0.5))
+			//(correctness)
+			val out:Manhattan = astar.best(new Manhattan(5,5))
+			out should be (new Manhattan(0,0))
+			//(time)
+			var count = astar.search(new Manhattan(5,5),BREAK)
+			count should be < (100)
+			//(mid time)
+			count = astar.search(new Manhattan(10,10),BREAK)
+			count should be < (2000)
+		}
+		it("works for Imprecise Manhattan"){
+			val astar:Search[EuclideanManhattan] = Search(memcap(A_STAR,10000,0.5))
+			//(long time)
+			val best:Manhattan = astar.best(new EuclideanManhattan(100,100))
+			best should be (new Manhattan(0,0))
+		}
+	}
+}
